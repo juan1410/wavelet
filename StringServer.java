10d3c2f9bfdbc713458afbc9a8363520f2622eda
juan1410 <jperezhernandez@ucsd.edu>
2023-04-24 13:59:55 -0700

    Create StringServer.java

---
+++ StringServer.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.net.URI;
+
+class StringServer implements URLHandler {
+    String str = "";
+
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return String.format(str);
+        } else {
+            System.out.println("Path: " + url.getPath());
+            if (url.getPath().contains("/add-message")) {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("s")) {
+                    str = str + parameters[1] + "\n";
+                    return String.format(str);
+                }
+            }
+            return "404 Not Found!";
+        }
+    }
+}
+
+class NumberServer {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new StringServer());
+    }
+}
